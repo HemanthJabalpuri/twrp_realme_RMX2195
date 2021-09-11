1c1a3f243f2abfdbc08f4f7b674edde1ab724e97
Ctapchuk <pilipenko0409@gmail.com>
2021-09-11 13:50:20 +0530

    RMX2195: Enable decryption support - Use bins, libs and manifest from UI1 - Use android-10 qcom-common decryption

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,6 +128,26 @@ TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 20.1.0
+TW_INCLUDE_CRYPTO := true
+BOARD_USES_QCOM_FBE_DECRYPTION := true
+
+# Take a few libraries from sources
+TARGET_RECOVERY_DEVICE_MODULES += \
+    ashmemd_aidl_interface-cpp \
+    libashmemd_client \
+    libdrm \
+    libion \
+    libxml2
+
+TW_RECOVERY_ADDITIONAL_RELINK_BINARY_FILES += \
+    $(TARGET_OUT_EXECUTABLES)/ashmemd
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/ashmemd_aidl_interface-cpp.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libashmemd_client.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libdrm.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
 
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
